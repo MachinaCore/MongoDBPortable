8b5c5f5713235a2bb7795b132ff657da36e2bc89
LORDofDOOM <tb@bbz-int.de>
2016-01-19 16:00:48 +0100

    [Core] Postgre to VS Project, fix creation of odoo user 8managed in odoo itself, change inin files to definied  values

---
+++ Other/Source/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PortableApps.com Installation Helper")]
+[assembly: AssemblyDescription("Simple Fake exe to run .paf.exe file silently")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("PortableApps.com")]
+[assembly: AssemblyProduct("PortableApps.com Platform")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25bcd104-5d7a-4256-8050-a24dea324d31")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Other/Source/README.txt
@@ -0,0 +1,2 @@
+Compile as X86 only !
+Dont works with AnyCPU
