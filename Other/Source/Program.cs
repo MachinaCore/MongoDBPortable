0778c50f8d166b7e67c0f698e5746b267a9acd62
LORDofDOOM <tb@bbz-int.de>
2017-11-03 06:04:57 +0100

    [Core] Add PgSQL 10.0.2 Binaries, upgrade Launcher

--- Other/Source/Program.cs
+++ Other/Source/Program.cs
@@ -9,7 +9,7 @@ using System.Windows.Forms;
 using CommandLine;
 using CommandLine.Text;
 using SharpConfig;
-
+using GathSystemsPortableApp;
 
 namespace Launcher
 {
@@ -19,230 +19,45 @@ namespace Launcher
         static void Main(string[] args)
         {
             //Check for Launcher ini file
-            Launcher.ReadLauncherIni();
+            PortableApp.Init();
+
+            //Set Stop Arguments
+            Globals.StopFileName = Globals.EnvironmentVariable["PGSQL"] + "\\bin\\pg_ctl.exe";
+            Globals.StopArguments = " stop --pgdata " + Globals.EnvironmentVariable["PGDATA"]
+                                  + " -l " + Globals.EnvironmentVariable["PGLOG"]
+                                  + " --mode=smart -W";
 
 
-            string pathvar = Environment.GetEnvironmentVariable("PATH");
-
-            foreach (var section in Globals.Launcher["Environment"])
+            //Create Database if not exists
+            if (!Directory.Exists(Globals.EnvironmentVariable["PGDATA"]))
             {
-                string sectionComment = null;
-                if (section.Comment != null)
-                {
-                    sectionComment = section.Comment.ToString();
-                }
-                string sectionString = section.StringValue + sectionComment;
-
-                sectionString = sectionString.Replace("%PATH%", pathvar);
-                sectionString = sectionString.Replace("%PAL:AppDir%", Globals.AppPath + "\\App");
-                sectionString = sectionString.Replace("%PAL:DataDir%", Globals.DataPath);
-
-                if (section.Name == "PGSQL")
-                    Globals.PostgreSQLEnvironment["PGSQL"] = sectionString;
-
-                if (section.Name == "PGDATA")
-                    Globals.PostgreSQLEnvironment["PGDATA"] = sectionString;
-
-                if (section.Name == "PGLOG")
-                    Globals.PostgreSQLEnvironment["PGLOG"] = sectionString;
-
-                if (section.Name == "PGLOCALEDIR")
-                    Globals.PostgreSQLEnvironment["PGLOCALEDIR"] = sectionString;
-
-                if (section.Name == "PGDATABASE")
-                    Globals.PostgreSQLEnvironment["PGDATABASE"] = sectionString;
-
-                if (section.Name == "PGPORT")
-                    Globals.PostgreSQLEnvironment["PGPORT"] = sectionString;
-
-                if (section.Name == "PGUSER")
-                    Globals.PostgreSQLEnvironment["PGUSER"] = sectionString;
-
-
-                Environment.SetEnvironmentVariable(section.Name, sectionString);
-                //Console.WriteLine(section.Name);
-                //Console.WriteLine(section.StringValue.Replace("%PAL:AppDir%", Globals.AppPath));
-                //Console.WriteLine(sectionString);
+                Globals.StartFileName = Globals.EnvironmentVariable["PGSQL"] + "\\bin\\initdb.exe";
+                Globals.StartArguments = " -U " + Globals.EnvironmentVariable["PGUSER"]
+                                         + " -A trust"
+                                         + " -E utf8"
+                                         + " --locale=C";
+                Console.WriteLine("Database creation ready");
+                PortableApp.Run(args);
             }
 
-            if (args != null)
-            {
-                if (args.Length != 0)
-                {
-                    for (int i = 0; i < args.Length; i++) // Loop through array
-                    {
-                        string argument = args[i];
-                        if (argument == "stop")
-                        {
-                            Console.WriteLine("Stopping PostgreSQL"); // Check for null array
-                            using (Process process = new Process())
-                            {
-                                process.StartInfo.FileName = Globals.PostgreSQLEnvironment["PGSQL"] + "\\bin\\pg_ctl.exe";
-                                process.StartInfo.Arguments = (" stop --pgdata " + Globals.PostgreSQLEnvironment["PGDATA"]
-                                                                + " -l " + Globals.PostgreSQLEnvironment["PGLOG"]
-                                                                + " --mode=smart -W"
-                                                                );
-                                process.Start();
-                            }
-                            return;
-                        }
-                        if (argument == "restart")
-                        {
-                            Console.WriteLine("Restarting PostgreSQL"); // Check for null array
-                            using (Process process = new Process())
-                            {
-                                process.StartInfo.FileName = Globals.PostgreSQLEnvironment["PGSQL"] + "\\bin\\pg_ctl.exe";
-                                process.StartInfo.Arguments = (" stop --pgdata " + Globals.PostgreSQLEnvironment["PGDATA"]
-                                                                + " -l " + Globals.PostgreSQLEnvironment["PGLOG"]
-                                                                + " --mode=smart -W"
-                                                                );
-                                process.StartInfo.CreateNoWindow = false;
-                                process.StartInfo.UseShellExecute = false;
-                                process.Start();
-                                process.WaitForExit();
-                                Thread.Sleep(3000);
-                            }
-                        }
-                    }
-                }
-            }
+            //Start Database
+            Globals.StartFileName = Globals.EnvironmentVariable["PGSQL"] + "\\bin\\pg_ctl.exe";
+            Globals.StartArguments = " -D " + Globals.EnvironmentVariable["PGDATA"]
+                                    + " -l " + Globals.EnvironmentVariable["PGLOG"]
+                                    + " -w start";
+
+            Console.WriteLine("Starting PostgreSQL");
+
+            PortableApp.Run(args);
 
 
-            //Check if running or definied Handler
-            if (Classes.AlreadyRunning.Handler())
-            {
-                return;
-            }
-
-            //Check if logfile exist -> Rename
-            if (File.Exists(Globals.AppPath + "\\" + Globals.ExeFileName.Replace(".exe", ".old.log")))
-            {
-                File.Delete(Globals.AppPath + "\\" + Globals.ExeFileName.Replace(".exe", ".old.log"));
-            }
-            if (File.Exists(Globals.AppPath + "\\" + Globals.ExeFileName.Replace(".exe", ".log")))
-            {
-                File.Move(Globals.AppPath + "\\" + Globals.ExeFileName.Replace(".exe", ".log"), Globals.AppPath + "\\" + Globals.ExeFileName.Replace(".exe", ".old.log"));
-            }
 
 
-            try
-            {
-                Globals.streamWriter = new StreamWriter(Globals.AppPath + "\\" + Globals.ExeFileName.Replace(".exe", "") + ".log", true);
-            }
-            catch
-            {
-                Console.WriteLine("Cant access log file -> Ignoring");
-            }
 
-            bool newInstance = true;
-            bool newInstallation = false;
-
-            using (Mutex mutex = new Mutex(true, Process.GetCurrentProcess().ProcessName, out newInstance))
-            {
-                if (!Directory.Exists(Globals.PostgreSQLEnvironment["PGDATA"]))
-                {
-                    newInstallation = true;
-                    //Creating Database
-                    using (Process process = new Process())
-                    {
-                        process.StartInfo.FileName = Globals.PostgreSQLEnvironment["PGSQL"] + "\\bin\\initdb.exe";
-                        process.StartInfo.Arguments = (" -U " + Globals.PostgreSQLEnvironment["PGUSER"]
-                                                        + " -A trust"
-                                                        + " -E utf8"
-                                                        + " --locale=C"
-                                                        );
-                        process.StartInfo.RedirectStandardError = true;
-                        //process.StartInfo.RedirectStandardInput = true;
-                        process.StartInfo.RedirectStandardOutput = true;
-                        process.StartInfo.UseShellExecute = false;
-                        process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
-
-                        process.OutputDataReceived += CaptureOutput;
-                        process.ErrorDataReceived += CaptureError;
-
-                        process.Start();
-                        process.BeginOutputReadLine();
-                        process.BeginErrorReadLine();
-                        process.WaitForExit();
-
-                        Console.WriteLine("Database creation ready");
-                    }
-                }
-
-                using (Process process = new Process())
-                {
-                    process.StartInfo.FileName = Globals.PostgreSQLEnvironment["PGSQL"] + "\\bin\\pg_ctl.exe";
-                    process.StartInfo.Arguments = (" -D " + Globals.PostgreSQLEnvironment["PGDATA"]
-                                                    + " -l " + Globals.PostgreSQLEnvironment["PGLOG"]
-                                                    + " -w start"
-                                                    );
-                    process.StartInfo.RedirectStandardError = true;
-                    //process.StartInfo.RedirectStandardInput = true;
-                    process.StartInfo.RedirectStandardOutput = true;
-                    process.StartInfo.UseShellExecute = false;
-                    process.StartInfo.StandardOutputEncoding = Encoding.UTF8;
-
-                    process.OutputDataReceived += CaptureOutput;
-                    process.ErrorDataReceived += CaptureError;
-
-                    process.Start();
-                    process.BeginOutputReadLine();
-                    process.BeginErrorReadLine();
-
-
-                    process.WaitForExit();
-
-                    Console.WriteLine("Starting PostgreSQL");
-                }
-            }
         }
 
-        static void CaptureOutput(object sender, DataReceivedEventArgs e)
-        {
-            ShowOutput(e.Data, ConsoleColor.Green);
-        }
-
-
-        static void CaptureError(object sender, DataReceivedEventArgs e)
-        {
-            ShowOutput(e.Data, ConsoleColor.Red);
-        }
-
-        static void ShowOutput(string data, ConsoleColor color)
-        {
-            if (data != null)
-            {
-                try
-                {
-                    Globals.streamWriter.WriteLine("[{0}][{1}] {2} ", String.Format("{0:yyyyMMdd HH:mm:ss}", DateTime.Now), "Info", data);
-                    Globals.streamWriter.Flush();
-                    ConsoleColor oldColor = Console.ForegroundColor;
-                    Console.ForegroundColor = color;
-                    Console.WriteLine("Received: {0}", data);
-                    Console.ForegroundColor = oldColor;
-                }
-                catch
-                {
-                    Console.WriteLine("Cant access log file -> Ignoring");
-                }
-
-            }
-        }
 
     }
 
 
-
-    public class Globals
-    {
-        public static Dictionary<string, string> PostgreSQLEnvironment = new Dictionary<string, string>();
-        public static string ExeFile = Assembly.GetExecutingAssembly().Location;
-        public static string ExeFileName = Path.GetFileName(ExeFile);
-        public static string AppPath = Path.GetDirectoryName(Globals.ExeFile);
-        public static string BasePath = Directory.GetParent(Path.GetDirectoryName(Globals.ExeFile)).FullName;
-        public static string DataPath = Globals.AppPath + "\\Data";
-        public static Configuration AppInfo = Configuration.LoadFromFile(AppPath + "\\App\\AppInfo\\AppInfo.ini");
-        public static Configuration Launcher = null;
-        public static StreamWriter streamWriter = null;
-    }
 }
